33128799951529b56103272db8d22eacb9898380
Colin Grambow <cgrambow@mit.edu>
2019-04-03 20:17:00 -0400

    Remove unnecessary imports

--- textgenrnn/model_training.py
+++ textgenrnn/model_training.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from keras.callbacks import LearningRateScheduler, Callback
-from keras.models import Model, load_model
 from keras.preprocessing import sequence
-from keras.preprocessing.text import Tokenizer, text_to_word_sequence
-from keras.utils import Sequence
-from keras import backend as K
+from keras.preprocessing.text import Tokenizer
 from utils import textgenrnn_encode_cat
 import numpy as np
 
